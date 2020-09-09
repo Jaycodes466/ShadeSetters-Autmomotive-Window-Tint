ac46c22effcc804a98d3be2461f70bc9aa497c78
Jaycodes466 <jaycodes@tutanota.com>
2020-09-08 21:38:44 -0600

    Fourth commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector('.custom-nav');
 window.onscroll = () => {
-    if (window.scrollY > 300) {
+    if (window.scrollY > 100) {
         navbar.classList.add('nav-active');
     } else {
         navbar.classList.remove('nav-active');
