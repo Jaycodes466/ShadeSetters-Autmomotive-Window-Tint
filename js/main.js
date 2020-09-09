0130c88dd6ff22760d6e72bbbdd6f52a2fa67590
Jaycodes466 <jaycodes@tutanota.com>
2020-09-08 22:54:37 -0600

    Fifth commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Nav change color on scroll
 const navbar = document.querySelector('.custom-nav');
 window.onscroll = () => {
     if (window.scrollY > 100) {
@@ -6,3 +7,13 @@ window.onscroll = () => {
         navbar.classList.remove('nav-active');
     }
 };
+
+// Mobile nav dissapears after clicked
+document.addEventListener("click", function(event) {
+    if (event.target.classList.contains("navbar-toggler-icon")) {
+      document.getElementById("responsiveNav").classList.toggle("show");
+    } else if (event.target.classList.contains("nav-link")) {
+      document.getElementById("responsiveNav").classList.remove("show");
+    }
+  });
+  
